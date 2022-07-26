d09f85f0e539139fd03446435625527ccb086d74
John r <elcorreo@gmal.com>
2022-07-26 12:21:19 -0500

    primera trepada o rama inicial

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpack    = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin');
+
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports={
+
+    mode:'development',
+     output:{
+        clean: true
+     },
+
+module:{
+
+    rules:[
+        {
+            test: /\.html$/,
+            loader: 'html-loader',
+            options:{
+                sources: false
+            }
+        },
+        {
+
+             test: /\.css$/,
+             exclude: /styles.css$/,
+              use:['style-loader','css-loader'] 
+        },
+        {
+            test: /styles.css$/,
+            use: [ MiniCssExtract.loader, 'css-loader' ]
+        },
+        {
+           test: /\.(png)$/ 
+        }
+    ]
+},
+
+optimization:{},
+
+plugins:[
+    new HtmlWebpack(
+        {
+            title: 'Mi webpack app',
+            //filename: 'holamundo.html'
+            template:'./src/index.html'
+        }
+    ),
+
+    new MiniCssExtract(
+        {
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+new CopyPlugin({
+    patterns:[
+      {from:'src/assets/',to:'assets/' }
+    ]
+})
+
+]
+
+}
